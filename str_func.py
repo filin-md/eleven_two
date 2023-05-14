673ced17e3adb34460783fd32aefc8f1b2e0b79c
Filin Mikhail <filin.md9@gmail.com>
2023-05-14 22:24:16 +0300

    add docstring to str_upper function in file str_func. Add function title_word to str_func.py

--- str_func.py
+++ str_func.py
@@ -1,2 +1,10 @@
 def str_upper(string):
+    """Возвращает строку большими буквами"""
     return string.upper()
+
+def title_word(string):
+    """Делает заглавными первые буквы каждого слова в строке, поступившей на вход функции"""
+    title_words = []
+    for word in string.split():
+        title_words.append(word.title())
+    return " ".join(title_words)
